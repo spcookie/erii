cf950c5c9a2e0c796bcb559ef6b6b0d097b87700
weixiang.liu <weixiang.liu@mijahealth.com>
2026-03-25 20:50:58 +0800

    refactor(test): 调整测试代码中的导入路径
    - 将 EventBus 的导入路径从 uesugi.core.component 修改为 uesugi.common
    - 将 HistoryEntity 的导入路径从 uesugi.core.message.history 修改为 uesugi.common
    - 将 MessageType 的导入路径从 uesugi.core.message.history 修改为 uesugi.common
    - 统一测试代码中的依赖引用路径，提高代码维护性

--- erii-core/src/test/kotlin/ApplicationTest.kt
+++ erii-core/src/test/kotlin/ApplicationTest.kt
@@ -24,9 +24,9 @@ import net.mamoe.mirai.message.data.Face
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import top.mrxiaom.overflow.BotBuilder
-import uesugi.core.component.EventBus
-import uesugi.core.message.history.HistoryEntity
-import uesugi.core.message.history.MessageType
+import uesugi.common.EventBus
+import uesugi.common.HistoryEntity
+import uesugi.common.MessageType
 import kotlin.test.Test
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
