6d32e9c7e70380dd6ba38f45b18983acb114428b
weixiang.liu <weixiang.liu@mijahealth.com>
2026-05-13 16:23:06 +0800

    fix(config): 修正 JobRunr 仪表盘端口配置
    - 将仪表盘端口配置改为通过系统属性动态设置
    - 默认端口设置为 8000
    - 提高了配置的灵活性和可定制性

--- erii-core/src/main/kotlin/uesugi/config/JobRunrConfig.kt
+++ erii-core/src/main/kotlin/uesugi/config/JobRunrConfig.kt
@@ -16,7 +16,7 @@ class JobRunrConfig(
             .useJobActivator(Activator)
             .useStorageProvider(H2StorageProvider(dataSource))
             .useBackgroundJobServer()
-            .useDashboard()
+            .useDashboard(System.getProperty("jobrunr.dashboard.port", "8000").toInt())
             .initialize()
             .jobScheduler
     }
