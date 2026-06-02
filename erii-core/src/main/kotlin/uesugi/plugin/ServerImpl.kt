2e856e5dd86ce1c7936e884bba8a26a1e974d71a
咸小鱼 <1307083930@qq.com>
2026-06-02 23:59:37 +0800

    refactor(server): 优化 ServerImpl 中的 URL 构建逻辑
    - 使用 URLBuilder 构建完整 URL，包含协议、主机、端口及路径段
    - 删除 basePath 属性，改为通过 URLBuilder 动态生成路径
    - 引入 ConfigHolder 获取浏览器外部访问地址
    - 保持 route 配置中路径调整为插件名称子路径

--- erii-core/src/main/kotlin/uesugi/plugin/ServerImpl.kt
+++ erii-core/src/main/kotlin/uesugi/plugin/ServerImpl.kt
@@ -1,19 +1,26 @@
 package uesugi.plugin
 
+import io.ktor.http.*
 import io.ktor.serialization.jackson.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
+import uesugi.common.toolkit.ConfigHolder
 import uesugi.server.SystemConfigHolder
 import uesugi.spi.PluginDef
 import uesugi.spi.Server
 
 class ServerImpl(val defined: PluginDef) : Server {
 
-    override val port: Int
-        get() = _port
+    override val url: URLBuilder
+        get() = URLBuilder().apply {
+            protocol = URLProtocol.HTTP
+            host = ConfigHolder.getBrowserExternalUrl()
+            port = _port
+            pathSegments = listOf("plugin", defined.name)
+        }
 
     companion object {
         private val _port by lazy {
@@ -44,9 +51,6 @@ class ServerImpl(val defined: PluginDef) : Server {
             ref!!
         }
     }
-
-    override val basePath: String
-        get() = "/plugin/${defined.name}"
 
     override fun route(conf: Route.() -> Unit) {
         routeing.route("/${defined.name}") {
